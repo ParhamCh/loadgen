6f2618a0dd5a2b77a41a3a1366fa76a83a974272
ParhamCh <parham.ch@icloud.com>
2025-11-04 02:10:55 +0330

    feat(api): add basic HTTP server with root handler returning "hello api"

--- cmd/loadgen/main.go
+++ cmd/loadgen/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	// TODO: wire HTTP server and routes here (healthz/readyz and root).
-	fmt.Println("loadgen skeleton")
+	// Define handler for root path
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Set content type to text/plain
+		w.Header().Set("Content-Type", "text/plain")
+		// Write response
+		fmt.Fprintln(w, "hello api")
+	})
+
+	// Start HTTP server on port 8080
+	fmt.Println("Server is running on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
 
